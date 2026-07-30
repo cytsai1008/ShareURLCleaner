837412269a0a66c91a2cbb9aa7c5ae6700846373
CYTsai <git@mail.photocat.blue>
2026-07-30 10:58:08 +0800

    chore: update version code for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.cytsai.urlclean"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
